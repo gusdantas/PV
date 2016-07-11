abe5d699abeb7d2f738e4dda140ed80c011dd26d
gusdantas <hdant5@gmail.com>
2016-07-11 20:57:43 -0300

    Update imageExample.cpp

--- imageExample.cpp
+++ imageExample.cpp
@@ -30,8 +30,8 @@ Mat media(int aresta, Mat input){
 	int linha = input.rows;
 	int coluna = input.cols;
 	vector< vector<int> > janela;
-	vector<int> vetorMediana;
-	vetorMediana.resize(aresta*aresta);
+	//vector<int> vetorMediana;
+	//vetorMediana.resize(aresta*aresta);
 
 	janela.resize(aresta);
 	for (int i=0; i < aresta; i++){
@@ -43,31 +43,32 @@ Mat media(int aresta, Mat input){
 
 			int posMediana = 0;
 			int somaJanela = 0;
+			int ij = 0;
 
 			for (int x=-meio; x<meio+1; x++){
-				int ij = 0;
+				int jj = 0;
 				for (int y=-meio; y<meio+1; y++){
-					int jj = 0;
+					
 					if (i+x < 0 || i+x >= linha || j+y < 0 || j+y >= coluna){
 						janela[ij][jj] = 0;
 					}else {
 						janela[ij][jj] = input.at<uchar>(Point(i+x,j+y));
 						somaJanela += janela[ij][jj];
 					}
-					vetorMediana[posMediana] = janela[ij][jj];
-					posMediana++;
+					//vetorMediana[posMediana] = janela[ij][jj];
+					//posMediana++;
 					jj++;
 				}
 				ij++;
 			}
-			//sort(vetorMediana.begin(),vetorMediana.end());
+			/*sort(vetorMediana.begin(),vetorMediana.end());
 			int outputMediana = vetorMediana[meio+1];
 			for (int v=0;v!=(aresta*aresta);v++){
 				cout << vetorMediana[v]<< " ";
 			}
-			cout<< "=> "<<outputMediana<<endl;
-			//int outputMedia = somaJanela/(aresta*aresta);
-			output.at<uchar>(Point(i,j)) = outputMediana;
+			cout<< "=> "<<outputMediana<<endl;*/
+			int outputMedia = somaJanela/(aresta*aresta);
+			output.at<uchar>(Point(i,j)) = outputMedia;
 		}
 	}
 	return output;
@@ -89,22 +90,21 @@ int main()
     namedWindow( "Read, Show and Save Image - window", WINDOW_AUTOSIZE ); // Create a window for display.
     imshow( "Read, Show and Save Image - window", image );
 
-	Mat blurred = image;
+	Mat blurred = image;	
+	//blur(image,blurred,Size(7,7));
+	medianBlur(image,blurred,7);
+	namedWindow( "OPENCV", WINDOW_AUTOSIZE ); // Create a window for display.
+    imshow( "OPENCV", blurred );
+    imwrite( "opencv.jpg", blurred ); // save your image with another name.
 
-	//medianBlur(image,blurred,3);
+    //newImage = media(7,image);
+    //namedWindow( "MEU", WINDOW_AUTOSIZE ); // Create a window for display.
+    //imshow( "MEU", newImage );
+    //imwrite( "meu.jpg", newImage ); // save your image with another name.
 
-    newImage = media(3,image);
-
-    namedWindow( "Filter", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Filter", newImage );
-
-    //namedWindow( "Blur", WINDOW_AUTOSIZE ); // Create a window for display.
-    //imshow( "Blur", blurred );
-
-    imwrite( "novaLena.jpg", image ); // save your image with another name.
+    //imwrite( "novaLena.jpg", image ); // save your image with another name.
 
     waitKey(0); // Wait for a keystroke in the window, Esc for exit.
     return 0;
 }
-
 
